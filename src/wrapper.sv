3314b97f3c1406d8b9cb80fcba68dce499263531
Stork1323 <tamd132003@gmail.com>
2024-04-07 00:17:55 +0700

    updated

--- src/wrapper.sv
+++ src/wrapper.sv
@@ -1,6 +1,7 @@
 module wrapper(
 	input logic CLOCK_50,
 	input logic [17:0] SW,
+	input logic [0:0] KEY,
 	output logic [16:0] LEDR,
 	output logic [7:0] LEDG,
 	output logic [6:0] HEX0,
@@ -18,23 +19,25 @@ module wrapper(
 	output logic [7:0] LCD_DATA
 	);
 	
+	logic [31:0] temp_r;
 	
+	//assign SW[16] = 1'b0;
 	
 	singlecycle CPU(
 		.clk_i(CLOCK_50),
 		.rst_ni(SW[17]),
-		.io_sw_i({{15{1'b0}}, SW[16:0]}),
-		.io_lcd_o({LCD_ON, {20{1'b0}}, LCD_EN, LCD_RS, LCD_RW, LCD_DATA}),
-		.io_ledg_o({{24{1'b0}}, LEDG}),
-		.io_ledr_o({{15{1'b0}}, LEDR}),
-		.io_hex0_o({{25{1'b0}} ,HEX0}),
-		.io_hex1_o({{25{1'b0}} ,HEX1}),
-		.io_hex2_o({{25{1'b0}} ,HEX2}),
-		.io_hex3_o({{25{1'b0}} ,HEX3}),
-		.io_hex4_o({{25{1'b0}} ,HEX4}),
-		.io_hex5_o({{25{1'b0}} ,HEX5}),
-		.io_hex6_o({{25{1'b0}} ,HEX6}),
-		.io_hex7_o({{25{1'b0}} ,HEX7})
+		.io_sw_i({{15{1'b0}}, KEY[0], SW[15:0]}),
+		.io_lcd_o({LCD_ON, temp_r[19:0], LCD_EN, LCD_RS, LCD_RW, LCD_DATA}),
+		.io_ledg_o(LEDG),
+		.io_ledr_o(LEDR),
+		.io_hex0_o(HEX0),
+		.io_hex1_o(HEX1),
+		.io_hex2_o(HEX2),
+		.io_hex3_o(HEX3),
+		.io_hex4_o(HEX4),
+		.io_hex5_o(HEX5),
+		.io_hex6_o(HEX6),
+		.io_hex7_o(HEX7)
 		);
 	
 endmodule
